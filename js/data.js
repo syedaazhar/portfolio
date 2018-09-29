b3b96b60c7c998c30f145ff9bd750b452d04fed5
syedaazhar <syedaazhar19@gmail.com>
2018-09-29 05:38:09 +0500

    portfolio

---
+++ js/data.js
@@ -0,0 +1,83 @@
+let details = [
+    {
+      box: 'box-1',
+      skillsId: 'skills-img1',
+    },
+    {
+      box: 'box-2',
+      skillsId: 'skills-img2',
+    },
+    {
+      box: 'box-3',
+      skillsId: 'skills-img3',
+    }
+  ]
+
+  
+  let projectDet = [
+    {
+      proBox: ' pro-box-1',
+      proImg: 'project-img1',
+      webLink: 'new-weather-app',
+      webName: 'Weather Forcast',
+      repo: ''
+    },
+    {
+      proBox: ' pro-box-2',
+      proImg: 'project-img2',
+      webLink: 'news-app',
+      webName: 'News-app'
+  
+    },
+    {
+      proBox: ' pro-box-3',
+      proImg: 'project-img3',
+      webLink: 'web-whatsapp',
+      webName: 'Web-whatsapp'
+  
+    },
+    {
+      proBox: ' pro-box-4',
+      proImg: 'project-img4',
+      webLink: 'mobile-gallery',
+      webName: 'Mobile-gallery'
+  
+    },
+    {
+      proBox: ' pro-box-5',
+      proImg: 'project-img5',
+      webLink: 'new-project',
+      webName: 'WEDDING PLANNERS'
+  
+    },
+    {
+      proBox: ' pro-box-6',
+      proImg: 'project-img6',
+      webLink: 'mt-file',
+      webName: 'Making Life Matter'
+    }
+  ]
+
+  let eduDetails = [
+    {
+      circle : 'circle-1',
+      place : 'UNIVERSITY OF KARACHI',
+      qualification : 'Masters in Women Studies'
+    },
+    {
+      circle : 'circle-2',
+      place : 'KHATOON E PAKISTAN',
+      qualification : 'Intermediate'
+    },
+    {
+      circle : 'circle-3',
+      place : 'SUNFLOWE CHILDREN ACADEMY',
+      qualification : 'Matriculation'
+    },
+    {
+      circle : 'circle-4',
+      place : 'CIRCLE',
+      qualification : 'Web Development Certification'
+    }
+  ]
+  
